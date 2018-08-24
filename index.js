e0dc60c2118c2df89a1701ec4fc6b6c405b83103
Emre <kklemre@gmail.com>
2018-08-25 00:22:55 +0300

    update
    update

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+// basic express route for index
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+// socket connection log
 io.on('connection', function(socket) {
     console.log('A new WebSocket connection has been established');
 });
-io.on('close', function(socket) {
-    console.log('Closed web socket.');
-});
 
+// count stock price
 setInterval(function() {
   var stockprice = Math.floor(Math.random() * 1000);
   io.emit('stock price reyiz', stockprice);
